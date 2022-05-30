493737e028d1df3642f5dc052bc39b85ae59c91e
Deniz Kumral <80388251+dkumral@users.noreply.github.com>
2022-05-30 18:25:50 +0200

    personal website
    version: 30 May 2022

---
+++ others.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<title>deniz kumral</title>
+<meta charset="UTF-8">
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+
+
+<body class="body">
+
+<!-- Page Content -->
+<?php include("header.html") ?>
+<div id="home">
+
+<a href="https://puntodergi.com/author/denizkumral/"> <img width="90" height="40" src="punto.png" alt = "max planck" ></a>
+  
+  <h2> Müzik </h2>
+
+<p>   Mekansal ve kültürel dönüsümün müzik kimligi üzerine etkisi: Kadiköy Örnegi (Yayinlanmamis Lisans Tezi, Istanbul Bilgi Üniversitesi, 2012).
+  <a href = "https://www.dropbox.com/s/kflkgvxj7vcxyrt/Kumral_Deniz_Kadikoy_Sound.pdf?dl=0 "> online pdf</a> </p>
+
+
+<p> Jefferson Airplane grubunun seslendirdiği White Rabbit adlı şarkı, göründüğünden daha fazla anlam içeriyor.
+  <a href = "https://seyler.eksisozluk.com/alice-harikalar-diyarindanin-bir-cocuk-masali-olmadigini-anlatan-sarki-white-rabbit"> seyler.eksisozluk.com </a> </p>
+
+</div>
+
+<?php include("footer.html") ?>
+
+  </body>
+  </html>
