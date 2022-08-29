26a47a63d1038fb48f5e1519a4b777da7ce9944f
Deniz Kumral <80388251+dkumral@users.noreply.github.com>
2022-08-29 21:40:16 +0200

    updated August 2022

---
+++ others.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<title>deniz kumral</title>
+<meta charset="UTF-8">
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+
+
+<body class="body">
+
+<!-- Page Content -->
+<?php include("header.html") ?>
+<div id="home">
+
+
+<p> <b> MetGala 2019 üzerine,</b> <a href = "
+https://www.dropbox.com/s/havwp5rg5k8hcdf/Kumral_metgala.pdf?dl=0"> online pdf</a> </p>
+
+<p> <b> Netflix  Bandersnatch: İnteraktif film olmak yeterli mi?,</b> <a href = "
+https://www.dropbox.com/s/80ndta0i2z7tqvp/Kumral_netflix.pdf?dl=0"> online pdf</a> </p>
+
+<p> <b> DÖNÜŞ / THE RETURN: Baba ölmeden iktidar mümkün mü?,</b> (<i>Psikesinema</i>, Temmuz 2016), 
+<a href = "https://www.dropbox.com/s/u1i4kyi5cyz4nj8/Kumral_return.pdf?dl=0">online pdf</a> </p>
+
+
+<p><b> Mekansal ve kültürel dönüsümün müzik kimligi üzerine etkisi: Kadiköy Örnegi </b> (<i>Yayinlanmamis Lisans Tezi</i>, Istanbul Bilgi Üniversitesi, 2012). <a href = "https://www.dropbox.com/s/kflkgvxj7vcxyrt/Kumral_Deniz_Kadikoy_Sound.pdf?dl=0 "> online pdf</a> </p>
+
+<p> Jefferson Airplane grubunun seslendirdiği White Rabbit adlı şarkı, göründüğünden daha fazla anlam içeriyor. <a href = "https://seyler.eksisozluk.com/alice-harikalar-diyarindanin-bir-cocuk-masali-olmadigini-anlatan-sarki-white-rabbit"> seyler.eksisozluk.com </a> </p>
+
+</div>
+
+<?php include("footer.html") ?>
+
+  </body>
+  </html>
